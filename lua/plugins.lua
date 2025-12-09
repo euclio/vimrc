e9845477ca7e7c845021d20416f2755786abd318
Andy Russell <arussell123@gmail.com>
2025-12-09 14:23:21 -0500

    feat(lua): enable lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,6 +38,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 vim.lsp.enable({
   'basedpyright',
   'hls',
+  'lua_ls',
   'ruff',
   'rust_analyzer',
   'ts_ls',
@@ -45,6 +46,39 @@ vim.lsp.enable({
 vim.lsp.config('*', {
   capabilities = capabilities,
   on_attach = on_attach,
+})
+
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
 })
 
 require 'diagnosticls-configs'.init {
